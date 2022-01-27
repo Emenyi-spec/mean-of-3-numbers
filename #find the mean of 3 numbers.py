a566c8f93a44fff6398983055dfca896474d3c86
Emenyi Nelly <60070513+Emenyi-spec@users.noreply.github.com>
2022-01-27 21:49:28 +0100

    Create #find the mean of 3 numbers.py

---
+++ #find the mean of 3 numbers.py
@@ -0,0 +1,7 @@
+#find the mean of 3 numbers
+
+number1 = int(input("Please enter first number:"))
+number2 = int(input("Please enter second number:"))
+number3 = int(input("Please enter third number:"))
+result = (number1 + number2 + number3) / 3
+print("The mean of three numbers: " , result)
